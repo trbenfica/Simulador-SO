70a6fe39f853a21d5b6b7cae3ffac9a05b091d53
William1344 <75541781+William1344@users.noreply.github.com>
2021-08-15 22:37:49 -0300

    Add files via upload

---
+++ TIso/src/tiso/GeradorDeRequisitos.java
@@ -0,0 +1,16 @@
+package tiso;
+
+/**
+ * 
+ * @author WillianS
+ */
+
+public class GeradorDeRequisitos{
+    private int buffer; // tamanho max da fila circular
+    private int itens; // nº de itens não consumidos
+    private int max; 
+    private int min;
+
+    public void defineTamanho(){}
+    public Requisição gerarRequisicao(){}
+} 
