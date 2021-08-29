da2a7d64b279db3623fcbe47039ef8167a01fcf1
herbertcaetano <39100599+herbertcaetano@users.noreply.github.com>
2021-08-29 16:18:07 -0300

    Add files via upload
    heap

---
+++ TIso/src/tiso/Heap.java
@@ -0,0 +1,77 @@
+package tiso;
+
+import java.util.LinkedList;
+
+public class Heap {
+	
+	int heap[];
+	private int tamanhoHeap;
+	
+	
+	
+	public Heap(int tamanhoHeap) {
+		this.heap = new int[tamanhoHeap];
+		this.tamanhoHeap = tamanhoHeap;
+		
+		for(int i=0; i<tamanhoHeap; i++) {
+			
+			heap[i]= 0;
+		}
+		
+	}
+	public int getTamanhoHeap() {
+		return tamanhoHeap;
+	}
+	public void setTamanhoHeap(int tamanhoHeap) {
+		this.tamanhoHeap = tamanhoHeap;
+	}
+	
+	public void addHeap(int partVariavel, int index) {
+		
+		heap[index] = partVariavel;
+		
+		
+	}
+	public int removeHeap(int index) {
+		
+		int temp = -1;
+		
+		if(heap == null)
+		return temp;
+		
+		else {
+			temp = heap[index];
+			heap[index] = 0;
+			
+		}
+		
+		
+		return temp;
+		
+	}
+	
+	public int[] encontrarCampoVazio() {
+		
+		int indexs[] = new int[tamanhoHeap];
+		int j = 0;
+		
+		
+		if(heap == null)
+			return null;
+		
+		else {
+			for(int i=0; i<tamanhoHeap; i++) {
+				int temp;
+				temp = heap[i];
+				if(temp == 0) {
+					indexs[j] = i;
+					j++;
+					
+				}
+			}
+		}
+		
+		return indexs;
+	}
+	
+}
